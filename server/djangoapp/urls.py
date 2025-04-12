272f61e0a6944dba7a9d2ae191bf4de4ae5be20f
Abhinav Gupta <abhinavgupta1702@gmail.com>
2025-04-12 16:49:10 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -18,13 +18,13 @@ urlpatterns = [
 
     # paths to get dealers and dealer details
     path('get_dealers', views.get_dealerships, name='get_dealers'),
-    path('get_dealers/<str:state>', views.get_dealerships, 
+    path('get_dealers/<str:state>', views.get_dealerships,
          name='get_dealers_by_state'),
 
     # path to get specific dealer details and reviews
-    path('dealer/<int:dealer_id>', views.get_dealer_details, 
+    path('dealer/<int:dealer_id>', views.get_dealer_details,
          name='dealer_details'),
-    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, 
+    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews,
          name='dealer_reviews'),
 
     # path to add a review
